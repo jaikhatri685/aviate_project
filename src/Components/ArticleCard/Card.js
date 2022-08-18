110870332f30ff5cb075115473474192973080b4
Jai Khatri <f20190543@goa.bits-pilani.ac.in>
2022-08-19 00:18:11 +0530

    margin fix

--- src/Components/ArticleCard/Card.js
+++ src/Components/ArticleCard/Card.js
@@ -6,28 +6,29 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import Box from "@mui/material/Box"
+import Box from "@mui/material/Box";
+import { shadows } from '@mui/system';
 
 export default function MediaCard({item,index}) {
 
     const [showMore, setShowMore] = useState(true);
    
   return (
-    <Card sx={{ display: 'flex', alignItems:"center",...(index%2===0&&{flexDirection:{xs:"column",md:"row"}}),...(index%2===1 &&{flexDirection:{xs:"column",lg:"row-reverse"}}), borderRadius:"25px", marginBottom:{sm:"20px",md:"50px"}}} >
+    <Card sx={{padding:"20px",boxShadow: 2, display: 'flex', alignItems:"center",...(index%2===0&&{flexDirection:{xs:"column",md:"row"}}),...(index%2===1 &&{flexDirection:{xs:"column",lg:"row-reverse"}}), borderRadius:"25px" ,mb:6}} >
         
       <CardMedia
         component="img"
         image={process.env.PUBLIC_URL+item?.image}
         alt="image"
-        sx={{height:"100%", width: {xs:"100%",md:"30%"},  }}
+        sx={{boxShadow: 2,height:"100%", width: {xs:"100%",md:"30%"}, borderRadius:"25px"  }}
 
       />
-      <Box style={{padding:"20px" }}>
-      <CardContent >
+      <Box sx={{...(index%2===0 && {pl:6}),...(index%2===1 &&{pr:6}) }}>
+      <CardContent sx={{}}>
         <Typography gutterBottom variant="h4" component="div">
           {item?.heading}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
+        <Typography variant="h6" color="text.secondary">
           {showMore && item?.info.substring(0, 500)}
           {!showMore && item?.info}
         </Typography>
